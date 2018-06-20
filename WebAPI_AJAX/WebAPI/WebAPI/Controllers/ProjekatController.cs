7dd490faf680b6b861014a108042aa2ad297a3bc
boske-vu <boske_woo@live.co.uk>
2018-06-20 18:23:08 +0200

    xml i txt
    Citanje dispecera iz txt-a i cuvanje korisnika u listu i xml bazu

--- WebAPI_AJAX/WebAPI/WebAPI/Controllers/ProjekatController.cs
+++ WebAPI_AJAX/WebAPI/WebAPI/Controllers/ProjekatController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 using WebAPI.Models;
 
 namespace WebAPI.Controllers
@@ -37,7 +38,35 @@ namespace WebAPI.Controllers
                 }
             }
 
-            return View("PogresnaSifra");
+            foreach (Dispecer d in PostojeciKorisnici.ListaDispecera)
+            {
+                if (d.Korisnicko_ime == korisnicko_ime && d.Lozinka != password)
+                {
+                    return View("PogresnaSifra");
+                }
+                else if (d.Korisnicko_ime == korisnicko_ime && d.Lozinka == password)
+                {
+                    d.Ulogovan = true;
+                    HttpContext.Application["Dispecer"] = d;
+                    return View("dispecerView", d);
+                }
+            }
+
+            foreach (Vozac v in PostojeciKorisnici.ListaVozaca)
+            {
+                if (v.Korisnicko_ime == korisnicko_ime && v.Lozinka != password)
+                {
+                    return View("PogresnaSifra");
+                }
+                else if (v.Korisnicko_ime == korisnicko_ime && v.Lozinka == password)
+                {
+                    v.Ulogovan = true;
+                    HttpContext.Application["Vozac"] = v;
+                    return View("VozacView", v);
+                }
+            }
+
+            return View("NePostojiKorisnik");
         }
 
         public ActionResult Register(string ime, string prezime, string pol, string korisnicko_ime, string password, string email, string broj_telefona, string jmbg)
@@ -55,16 +84,72 @@ namespace WebAPI.Controllers
 
             Korisnik m = new Musterija(korisnicko_ime, password, ime, prezime, p, jmbg, broj_telefona, email, Uloge.Musterija);
 
-            foreach (Korisnik k in PostojeciKorisnici.ListaMusterija)
+            if (PostojeciKorisnici.ListaMusterija != null)
             {
-                if (k.Korisnicko_ime == m.Korisnicko_ime)
+                foreach (Korisnik k in PostojeciKorisnici.ListaMusterija)
                 {
-                    return View("KorisnikPostoji");
+                    if (k.Korisnicko_ime == m.Korisnicko_ime)
+                    {
+                        return View("KorisnikPostoji");
+                    }
                 }
             }
 
             PostojeciKorisnici.ListaKorisnika.Add(m);
             PostojeciKorisnici.ListaMusterija.Add(m as Musterija);
+
+                using (XmlWriter writer = XmlWriter.Create(@"C:\Users\HP\Desktop\Projakat\WP1718-PR101-2015\WebAPI_AJAX\WebAPI\WebAPI\baza.xml"))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Korisnici");
+
+                    foreach (Korisnik k in PostojeciKorisnici.ListaMusterija)
+                    {
+                        writer.WriteStartElement("Musterije");
+
+
+                    writer.WriteElementString("Ime", k.Ime);
+                    writer.WriteElementString("Prezime", k.Prezime);
+                    writer.WriteElementString("Pol", k.Pol.ToString());
+                    writer.WriteElementString("KorisnickoIme", k.Korisnicko_ime);
+                    writer.WriteElementString("Sifra", k.Lozinka);
+                    writer.WriteElementString("JMBG", k.Jmbg);
+                    writer.WriteElementString("KontaktTelefon", k.Kontakt_telefon);
+                    writer.WriteElementString("EMail", k.Email);
+                    writer.WriteElementString("Uloga", k.Uloga.ToString());
+
+                    writer.WriteEndElement();
+                    }
+
+                    /*
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                    */
+               
+                foreach (Korisnik k in PostojeciKorisnici.ListaDispecera)
+                {
+                    writer.WriteStartElement("Dispeceri");
+
+
+                    writer.WriteElementString("Ime", k.Ime);
+                    writer.WriteElementString("Prezime", k.Prezime);
+                    writer.WriteElementString("Pol", k.Pol.ToString());
+                    writer.WriteElementString("KorisnickoIme", k.Korisnicko_ime);
+                    writer.WriteElementString("Sifra", k.Lozinka);
+                    writer.WriteElementString("JMBG", k.Jmbg);
+                    writer.WriteElementString("KontaktTelefon", k.Kontakt_telefon);
+                    writer.WriteElementString("EMail", k.Email);
+                    writer.WriteElementString("Uloga", k.Uloga.ToString());
+
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+            
+
+
 
             return View("Registrovani", m);
 
--- WebAPI_AJAX/WebAPI/WebAPI/Global.asax.cs
+++ WebAPI_AJAX/WebAPI/WebAPI/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Xml;
 using WebAPI.Models;
 
 namespace WebAPI
@@ -19,9 +20,71 @@ namespace WebAPI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
+            
             PostojeciKorisnici pk = new PostojeciKorisnici();
-            PostojeciKorisnici.ListaMusterija.Add(new Musterija("boske", "fdjif", "fjdk", "bosanac", Pol.Muski, "2192301", "03293092", "bkfo@fko", Uloge.Musterija));
+            
+            if(PostojeciKorisnici.ListaDispecera.Count() == 0)
+            {
+                string line;
+                // Read the file and display it line by line.  
+                System.IO.StreamReader file =
+                    new System.IO.StreamReader(@"C:\Users\HP\Desktop\Projakat\WP1718-PR101-2015\WebAPI_AJAX\WebAPI\WebAPI\dispeceri.txt");
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] polja = line.Split(':');
+                    Dispecer d = new Dispecer();
+
+                    d.Korisnicko_ime = polja[0];
+                    d.Lozinka = polja[1];
+                    d.Ime = polja[2];
+                    d.Prezime = polja[3];
+                    if (polja[4].Equals("Muski"))
+                        d.Pol = Pol.Muski;
+                    else
+                        d.Pol = Pol.Zenski;
+                    d.Jmbg = polja[5];
+                    d.Kontakt_telefon = polja[6];
+                    d.Email = polja[7];
+                    d.Uloga = Uloge.Dispecer;
+
+                    PostojeciKorisnici.ListaKorisnika.Add(d);
+                    PostojeciKorisnici.ListaDispecera.Add(d);
+
+                }
+
+                file.Close();
+            }
+
+            using (XmlWriter writer = XmlWriter.Create(@"C:\Users\HP\Desktop\Projakat\WP1718-PR101-2015\WebAPI_AJAX\WebAPI\WebAPI\baza.xml"))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Korisnici");
+
+                foreach (Korisnik k in PostojeciKorisnici.ListaKorisnika)
+                {
+                    writer.WriteStartElement("Korisnici");
+
+
+                    writer.WriteElementString("Ime", k.Ime);
+                    writer.WriteElementString("Prezime", k.Prezime);
+                    writer.WriteElementString("Pol", k.Pol.ToString());
+                    writer.WriteElementString("KorisnickoIme", k.Korisnicko_ime);
+                    writer.WriteElementString("Sifra", k.Lozinka);
+                    writer.WriteElementString("JMBG", k.Jmbg);
+                    writer.WriteElementString("KontaktTelefon", k.Kontakt_telefon);
+                    writer.WriteElementString("EMail", k.Email);
+                    writer.WriteElementString("Uloga", k.Uloga.ToString());               
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+            // Read the file as one string.
+            string text = System.IO.File.ReadAllText(@"C:\Users\HP\Desktop\Projakat\WP1718-PR101-2015\WebAPI_AJAX\WebAPI\WebAPI\dispeceri.txt");
+
+            System.IO.File.WriteAllText(@"C:\Users\HP\Desktop\Projakat\WP1718-PR101-2015\WebAPI_AJAX\WebAPI\WebAPI\upis.txt", text);
         }
     }
 }
